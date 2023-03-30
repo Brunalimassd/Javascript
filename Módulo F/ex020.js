671fe9db49fad7f976c9d8fd884d3668eacb465d
Bruna Lima <brubsbsb270@gmail.com>
2023-03-30 15:01:31 -0300

    chore: initial project

---
+++ Módulo F/ex020.js
@@ -0,0 +1,8 @@
+let num  = [5, 8, 9, 6]
+num.push(3)
+
+console.log(num)
+console.log(`O vetor tem ${num.length} indices `)
+
+let pos = num.indexOf(5)
+console.log(`O valor 5 esta na posição ${pos}`)
